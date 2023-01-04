e9f2b8741e5f19701cfa3effbb1831281ec2d9db
Adarsha Acharya <adarshrya@gmail.com>
2023-01-05 04:05:53 +0530

    Fix

--- FE/src/http-common.ts
+++ FE/src/http-common.ts
@@ -4,10 +4,5 @@ export default axios.create({
     baseURL: "/api",
     headers: {
         "Content-type": "application/json"
-    },
-    proxy: {
-        protocol: 'https',
-        host: 'todosvc.onrender.com',
-        port: 443,
     }
 });
