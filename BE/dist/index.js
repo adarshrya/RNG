d023bf35691730482d79bb3504648c7fa496327a
Adarsha Acharya <adarshrya@gmail.com>
2023-01-05 01:20:41 +0530

    Init

---
+++ BE/dist/index.js
@@ -0,0 +1,92 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const client_1 = require("@prisma/client");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.use(express_1.default.json());
+const prisma = new client_1.PrismaClient();
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.post(`/todo`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { text } = req.body;
+    const result = yield prisma.todo.create({
+        data: {
+            created_at: new Date(),
+            done: false,
+            text: text,
+        },
+    });
+    res.json(result);
+}));
+app.put('/todo/:id/edit', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const { text } = req.body;
+    try {
+        var post = yield prisma.todo.update({
+            data: {
+                text: text,
+            },
+            where: {
+                uid: id
+            }
+        });
+        res.json(post);
+    }
+    catch (error) {
+        res.json({ error: `Post with ID ${id} does not exist in the database` });
+    }
+}));
+app.put('/todo/:id/done', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+        var post = yield prisma.todo.update({
+            data: {
+                done: true,
+            },
+            where: {
+                uid: id
+            }
+        });
+        res.json(post);
+    }
+    catch (error) {
+        res.json({ error: `Post with ID ${id} does not exist in the database` });
+    }
+}));
+app.delete('/todo/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+        var post = yield prisma.todo.delete({
+            where: {
+                uid: id
+            }
+        });
+        res.json(post);
+    }
+    catch (error) {
+        res.json({ error: `todo with ID ${id} does not exist in the database` });
+    }
+}));
+app.get('/todo', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var posts = yield prisma.todo.findMany();
+    res.json(posts);
+}));
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ BE/index.ts
@@ -0,0 +1,83 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { Prisma, PrismaClient } from '@prisma/client'
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+app.use(express.json())
+const prisma = new PrismaClient()
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.post(`/todo`, async (req, res) => {
+    const { text } = req.body
+    const result = await prisma.todo.create({
+        data: {
+            created_at: new Date(),
+            done: false,
+            text: text,
+        },
+    })
+    res.json(result)
+})
+
+app.put('/todo/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const { text } = req.body
+    try {
+        var post = await prisma.todo.update({
+            data: {
+                text: text,
+            },
+            where: {
+                uid: id
+            }
+        })
+        res.json(post)
+    } catch (error) {
+        res.json({ error: `Post with ID ${id} does not exist in the database` })
+    }
+})
+
+app.put('/todo/:id/done', async (req, res) => {
+    const { id } = req.params
+    try {
+        var post = await prisma.todo.update({
+            data: {
+                done: true,
+            },
+            where: {
+                uid: id
+            }
+        })
+        res.json(post)
+    } catch (error) {
+        res.json({ error: `Post with ID ${id} does not exist in the database` })
+    }
+})
+
+app.delete('/todo/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        var post = await prisma.todo.delete({
+            where: {
+                uid: id
+            }
+        })
+        res.json(post)
+    } catch (error) {
+        res.json({ error: `todo with ID ${id} does not exist in the database` })
+    }
+})
+
+app.get('/todo', async (req, res) => {
+    var posts = await prisma.todo.findMany()
+    res.json(posts)
+})
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
